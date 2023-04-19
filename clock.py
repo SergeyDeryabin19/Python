85348684cd5103d6a7da52dbfb35e8e451e93351
SergeyDeryabin19 <deryabinsergey1994@gmail.com>
2023-04-20 01:00:06 +0300

    new update new output

--- clock.py
+++ clock.py
@@ -4,25 +4,94 @@ import time
 
 
 
-dict_nombers={0: "zero",
-              1: "one",
-              2: "two",
-              3: "three",
-              4: "four",
-              5: "five",
-              6: "six",
-              7: "seven",
-              8: "eight",
-              9: "nine",
-              }
+dict_nombers=\
+{0:\
+    """
+    ■■■■■
+    ■   ■
+    ■   ■
+    ■   ■
+    ■■■■■""",
+1:\
+    """
+      ■  
+     ■■  
+    ■ ■  
+      ■  
+    ■■■■■""",
+2:\
+    """
+    ■■■■■
+    ■   ■
+      ■  
+    ■    
+    ■■■■■""",
+3:\
+    """
+    ■■■■ 
+        ■
+      ■  
+        ■
+    ■■■■ """,
+4:\
+    """
+    ■   ■
+    ■   ■
+    ■ ■ ■
+        ■
+        ■""",
+5:\
+    """
+    ■■■■■
+    ■    
+    ■ ■ ■
+        ■
+    ■■■■■""",
+6:\
+    """
+    ■■■■■
+    ■    
+    ■ ■ ■
+    ■   ■
+    ■■■■■""",
+7:\
+    """
+    ■■■■■
+        ■
+      ■  
+      ■  
+      ■  """,
+8:\
+    """
+    ■■■■■
+    ■   ■
+    ■■■■■
+    ■   ■
+    ■■■■■""",
+9:\
+    """
+    ■■■■■
+    ■   ■
+    ■ ■ ■
+        ■
+    ■■■■■"""
+}
+
 
 
 def screen_clear():
     os.system("CLS")
     
 
-def print_result(continue_hour, continue_minute, continue_second):
-    print(f"{continue_hour} {continue_minute} {continue_second}")
+def print_result(continue_hour_1, continue_hour_2, continue_minute_1, continue_minute_2, continue_second_1, continue_second_2):
+    lines1 = continue_hour_1.splitlines()
+    lines2 = continue_hour_2.splitlines()
+    lines3 = continue_minute_1.splitlines()
+    lines4 = continue_minute_2.splitlines()
+    lines5 = continue_second_1.splitlines()
+    lines6 = continue_second_2.splitlines()
+    for i in range(len(lines1)):
+        print("{} {} {} {} {} {}".format(lines1[i], lines2[i], lines3[i], lines4[i], lines5[i], lines6[i]))
     
 
 def chek_zero(our_number):
@@ -42,55 +111,40 @@ def new_vew_hour(current_time_number):
     current_hour = str(current_time_number.hour)
     splited_hour = [int(a) for a in str(current_hour)]
     new_current_splited_hour=chek_zero(splited_hour)
-    new_current_hour=f"{dict_nombers[new_current_splited_hour[0]]} {dict_nombers[new_current_splited_hour[1]]}"
-    return new_current_hour
+    new_current_splited_hour_1=dict_nombers[new_current_splited_hour[0]]
+    new_current_splited_hour_2=dict_nombers[new_current_splited_hour[1]]
+    return new_current_splited_hour_1, new_current_splited_hour_2
     
 
 def new_vew_minute(current_time_number):
     current_minute = current_time_number.minute
     splited_minute = [int(a) for a in str(current_minute)]
     new_current_splited_minute=chek_zero(splited_minute)
-    new_current_minute=f"{dict_nombers[new_current_splited_minute[0]]} {dict_nombers[new_current_splited_minute[1]]}"
-    return new_current_minute
+    new_current_splited_minute_1=dict_nombers[new_current_splited_minute[0]]
+    new_current_splited_minute_2=dict_nombers[new_current_splited_minute[1]]
+    return new_current_splited_minute_1, new_current_splited_minute_2
     
 
 def new_vew_second(current_time_number):
     current_second = current_time_number.second
     splited_second = [int(a) for a in str(current_second)]
     new_current_splited_second=chek_zero(splited_second)
-    new_current_second=f"{dict_nombers[new_current_splited_second[0]]} {dict_nombers[new_current_splited_second[1]]}"
-    return new_current_second
+    new_current_splited_second_1=dict_nombers[new_current_splited_second[0]]
+    new_current_splited_second_2=dict_nombers[new_current_splited_second[1]]
+    return new_current_splited_second_1, new_current_splited_second_2
     
 
 def main():
     screen_clear()
     current_time_number = current_time()
-    continue_hour=new_vew_hour(current_time_number)
-    continue_minute=new_vew_minute(current_time_number)
-    continue_second=new_vew_second(current_time_number)
-    print_result(continue_hour, continue_minute, continue_second)
-    time.sleep(0.5)
-    
+    continue_hour_1, continue_hour_2=new_vew_hour(current_time_number)
+    continue_minute_1, continue_minute_2=new_vew_minute(current_time_number)
+    continue_second_1, continue_second_2=new_vew_second(current_time_number)
+    print_result(continue_hour_1, continue_hour_2, continue_minute_1, continue_minute_2, continue_second_1, continue_second_2)
+    time.sleep(1)
+    print()
     
     
        
 while True:
     main()
-
-
-
-
-
-
-
-
-
-
-# new_current_hour=dict_nombers[current_hour]
-# print(new_current_hour)
-
-
-
-# if d.hour == 14:
-#     print("""14
-#           """)
--- test_file.py
+++ test_file.py
@@ -1,12 +1,154 @@
-def chek_zero(our_number):
-    chek_len = len(str(our_number))
-    print(chek_len)
-    if chek_len==3:
-        firs_number = 0
-        second_number = our_number[0]
-        our_number=[firs_number, second_number]
-    
-    return print(our_number)
+dict_nombers=\
+{0:\
+    """
+    ■■■■■
+    ■   ■
+    ■   ■
+    ■   ■
+    ■■■■■""",
+1:\
+    """
+      ■  
+     ■■  
+    ■ ■  
+      ■  
+    ■■■■■""",
+2:\
+    """
+    ■■■■■
+    ■   ■
+      ■  
+    ■    
+    ■■■■■""",
+3:\
+    """
+    ■■■■ 
+        ■
+      ■  
+        ■
+    ■■■■ """,
+4:\
+    """
+    ■   ■
+    ■   ■
+    ■ ■ ■
+        ■
+        ■""",
+5:\
+    """
+    ■■■■■
+    ■    
+    ■ ■ ■
+        ■
+    ■■■■■""",
+6:\
+    """
+    ■■■■■
+    ■    
+    ■ ■ ■
+    ■   ■
+    ■■■■■""",
+7:\
+    """
+    ■■■■■
+        ■
+      ■  
+      ■  
+      ■  """,
+8:\
+    """
+    ■■■■■
+    ■   ■
+    ■■■■■
+    ■   ■
+    ■■■■■""",
+9:\
+    """
+    ■■■■■
+    ■   ■
+    ■ ■ ■
+        ■
+    ■■■■■"""
+}
 
-our_number=[5]
-chek_zero(our_number)
+def split_number(number):
+    list = []
+    for line in number.split("\n"):
+        if not line.strip():
+            continue
+        list.append(line.lstrip())
+    n_0, n_1, n_2, n_3, n_4 = list[0], list[1], list[2], list[3], list[4]
+    return n_0, n_1, n_2, n_3, n_4 
+
+def split_number_return_list(number):
+    list = []
+    for line in number.split("\n"):
+        if not line.strip():
+            continue
+        list.append(line.lstrip())
+    
+    return list
+
+
+def list_print(income_list):
+    print(income_list[0])
+    print(income_list[1])
+    print(income_list[2])
+    print(income_list[3])
+    print(income_list[4])
+    
+
+
+def print_spline_number(string_0, string_1, string_2, string_3, string_4):
+    print(string_0)
+    print(string_1)
+    print(string_2)
+    print(string_3)
+    print(string_4)
+    
+    
+
+# n_0, n_1, n_2, n_3, n_4 = split_number(dict_nombers[8])
+# print_spline_number(n_0, n_1, n_2, n_3, n_4) 
+
+
+
+def print_spline_number(string_0, string_1, string_2, string_3, string_4, string_5, string_6, string_7, string_8, string_9): 
+    lines_0 = string_0.splitlines()
+    lines_1 = string_1.splitlines()
+    lines_2 = string_2.splitlines()
+    lines_3 = string_3.splitlines()
+    lines_4 = string_4.splitlines()
+    lines_5 = string_5.splitlines()
+    lines_6 = string_6.splitlines()
+    lines_7 = string_7.splitlines()
+    lines_8 = string_8.splitlines()
+    lines_9 = string_9.splitlines()
+
+    for i in range(len(lines_2)):
+        print(lines_0[i] + "   " + lines_1[i]\
+            + "   " + lines_2[i]+ "   " + lines_3[i]\
+                + "   " + lines_4[i]+ "   " + lines_5[i]\
+                    + "   " + lines_6[i]+ "   " + lines_7[i]\
+                        + "   " + lines_8[i]+ "   " + lines_9[i])
+
+
+string_0, string_1, string_2, string_3, string_4,\
+    string_5, string_6, string_7, string_8, string_9=\
+        dict_nombers[0],dict_nombers[1],dict_nombers[2],dict_nombers[3],dict_nombers[4],\
+            dict_nombers[5],dict_nombers[6],dict_nombers[7],dict_nombers[8],dict_nombers[9]
+            
+print_spline_number(string_0, string_1, string_2, string_3, string_4,\
+    string_5, string_6, string_7, string_8, string_9)
+
+ 
+
+
+
+# a=dict_nombers[2].split(" ")
+# print(a[0])
+# b=","
+# print(b.join(a))
+
+# print(b.join(a),b.join(a))
+# print(dict_nombers[2].split("-"))
