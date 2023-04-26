1b033a3acb196fdb32b0af5fd7b2c62d3c7b5912
SergeyDeryabin19 <deryabinsergey1994@gmail.com>
2023-04-26 23:27:34 +0300

    new one commit

--- test_file_2.py
+++ test_file_2.py
@@ -1,51 +1,156 @@
-from random import randint
+from datetime import datetime
+import os
+import time
 
-# Создаем пустое поле 10х10
-board = []
-for i in range(10):
-    board.append(["O"] * 10)
 
-# Функция для печати текущего состояния поле
-def print_board(board):
-    for row in board:
-        print(" ".join(row))
+output_number =\
+{0:\
+    """
+    ■■■■■
+    ■   ■
+    ■   ■
+    ■   ■
+    ■■■■■""",
+1:\
+    """
+      ■  
+     ■■  
+    ■ ■  
+      ■  
+    ■■■■■""",
+2:\
+    """
+    ■■■■■
+    ■   ■
+      ■  
+    ■    
+    ■■■■■""",
+3:\
+    """
+    ■■■■ 
+        ■
+      ■  
+        ■
+    ■■■■ """,
+4:\
+    """
+    ■   ■
+    ■   ■
+    ■ ■ ■
+        ■
+        ■""",
+5:\
+    """
+    ■■■■■
+    ■    
+    ■ ■ ■
+        ■
+    ■■■■■""",
+6:\
+    """
+    ■■■■■
+    ■    
+    ■ ■ ■
+    ■   ■
+    ■■■■■""",
+7:\
+    """
+    ■■■■■
+        ■
+      ■  
+      ■  
+      ■  """,
+8:\
+    """
+    ■■■■■
+    ■   ■
+    ■■■■■
+    ■   ■
+    ■■■■■""",
+9:\
+    """
+    ■■■■■
+    ■   ■
+    ■ ■ ■
+        ■
+    ■■■■■""",
+"dot":\
+    """
+         
+      ■  
+         
+      ■  
+         """,
+"nothing":\
+    """
+         
+         
+         
+         
+         """
+    
+}
 
-# Генерируем случайную позицию корабля
-def random_row(board):
-    return randint(0, len(board) - 1)
+def clear_screen():
+  os.system('CLS')
 
-def random_col(board):
-    return randint(0, len(board[0]) - 1)
+def print_result(hour_1, hour_2, minute_1, minute_2, second_1, second_2):
+  line_1 = hour_1.splitlines()
+  line_2 = hour_2.splitlines()
+  line_3 = minute_1.splitlines()
+  line_4 = minute_2.splitlines()
+  line_5 = second_1.splitlines()
+  line_6 = second_2.splitlines()
+  dot = output_number['dot']
+  dot_new = dot.splitlines()
+  nothing = output_number['nothing']
+  nothing_new = nothing.splitlines()
+  if datetime.now().second % 2 == 0:
+    for i in range (len(line_1)):
+      print(f'{line_1[i]}, {line_2[i]}, {nothing_new[i]}, {line_3[i]}, {line_4[i]}, \
+            {nothing_new[i]}, {line_5[i]}, {line_6[i]}')
+  elif datetime.now().second % 2 != 0:
+    for i in range (len(line_1)):
+      print(f'{line_1[i]}, {line_2[i]}, {dot_new[i]}, {line_3[i]}, {line_4[i]}, \
+            {dot_new[i]}, {line_5[i]}, {line_6[i]}')
+  
+def null_check(our_number):
+  check_lenght = len(str(our_number))
+  if check_lenght == 3:
+    first_number = 0
+    second_number = our_number[0]
+    our_number = [first_number, second_number]
+  return our_number
 
-# Заполняем поле одним кораблем
-ship_row = random_row(board)
-print(ship_row)
-ship_col = random_col(board)
-print(ship_col)
+def new_hour(today_time):
+  split_hour = [int(a) for a in str(today_time.hour)]
+  new_split_hour = null_check(split_hour)
+  new_split_hour_1, new_split_hour_2 = output_number[new_split_hour[0]], \
+  output_number[new_split_hour[1]]
+  return new_split_hour_1, new_split_hour_2
 
-# Играем до тех пор, пока корабль не будет потоплен
-for turn in range(4):
-    print("Ход", turn + 1)
+def new_minute(today_time):
+  split_minute = [int(a) for a in str(today_time.minute)]
+  new_split_minute = null_check(split_minute)
+  new_split_minute_1, new_split_minute_2 = output_number[new_split_minute[0]], \
+  output_number[new_split_minute[1]]
+  return new_split_minute_1, new_split_minute_2
 
-    # Просим игрока выбрать строку и столбец
-    guess_row = int(input("Выберите строку (от 1 до 10): ")) - 1
-    guess_col = int(input("Выберите столбец (от 1 до 10): ")) - 1
+def new_second(today_time):
+  split_second = [int(a) for a in str(today_time.second)]
+  new_split_second = null_check(split_second)
+  new_split_second_1, new_split_second_2 = output_number[new_split_second[0]], \
+  output_number[new_split_second[1]]
+  return new_split_second_1, new_split_second_2
 
-    # Проверяем, попал ли игрок в корабль
-    if guess_row == ship_row and guess_col == ship_col:
-        print("Поздравляем, вы потопили корабль!")
-        break
-    else:
-        # Если игрок промахнулся, отмечаем клетку на поле
-        if (guess_row < 0 or guess_row > 9) or (guess_col < 0 or guess_col > 9):
-            print("Выстрел за пределами поля.")
-        elif board[guess_row][guess_col] == "X":
-            print("Вы уже стреляли в эту клетку.")
-        else:
-            print("Промах.")
-            board[guess_row][guess_col] = "X"
-        # Печатаем новое состояние поля
-        print_board(board)
-    # Если это был последний ход, сообщаем игроку, что он проиграл
-    if turn == 3:
-        print("Игра окончена. Вы проиграли.")
+def main():
+  clear_screen()
+  today_time=datetime.now()
+  hour_1, hour_2 = new_hour(today_time)
+  minute_1, minute_2 = new_minute(today_time)
+  second_1, second_2 = new_second(today_time)
+  print_result(hour_1, hour_2, minute_1, minute_2, second_1, second_2)
+  time.sleep(1)
+
+while True:
+  main()
